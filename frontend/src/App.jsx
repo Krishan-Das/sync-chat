07f4e0747c55da9fd1e71eba0f15a2cdb415c545
Krishan Das <krishan8974783135@gmail.com>
2026-07-01 17:47:04 +0530

    Setup react and tailwindcss

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-amber-800 text-white px-4 py-2 rounded-2xl mx-5 my-4'>
+      Sync Chat Frontend Project setup completed
+    </div>
+  )
+}
+
+export default App
